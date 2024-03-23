9007b6690ccd403bae767ae4c4fca1fb3b4210a8
Saptarshi Adhikari <66560136+Mastermind-sap@users.noreply.github.com>
2024-03-23 15:57:58 +0530

    memeapp

--- lib/homescreen.dart
+++ lib/homescreen.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -7,41 +10,33 @@ class HomeScreen extends StatefulWidget {
   State<HomeScreen> createState() => _HomeScreenState();
 }
 
-List<String> tasks = ["To learn flutter", "To code a game", "to code taskzen"];
+var meme =
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS21LMjafCwhc7p64MMA_Sx68WbK_FvJJk9VbQ9wPUsUA&s";
 
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.red,
-      appBar: AppBar(
-        title: Text('Task Zen'),
-        centerTitle: true,
-        backgroundColor: Colors.blue,
-      ),
-      body: ListView.builder(
-        itemCount: tasks.length,
-        itemBuilder: (context, index) => Text(
-          tasks[index],
-          style: TextStyle(fontSize: 32, color: Colors.white),
+        backgroundColor: Colors.red,
+        appBar: AppBar(
+          title: Text('Memeify'),
+          centerTitle: true,
+          backgroundColor: Colors.blue,
         ),
-      ),
-      floatingActionButton: IconButton(
-          style: ButtonStyle(
-              iconSize: MaterialStatePropertyAll(100),
-              foregroundColor: MaterialStatePropertyAll(Colors.deepOrange),
-              backgroundColor: MaterialStateProperty.resolveWith(
-                (states) {
-                  print(states);
-                  if (states.contains(MaterialState.pressed)) {
-                    return Colors.yellow;
-                  } else {
-                    return Colors.blue;
-                  }
-                },
-              )),
-          onPressed: () {},
-          icon: Icon(Icons.add)),
-    );
+        body: Center(
+          child: Container(
+            height: MediaQuery.of(context).size.height * .9,
+            width: MediaQuery.of(context).size.width * .9,
+            child: Image(image: NetworkImage(meme)),
+          ),
+        ),
+        floatingActionButton: ElevatedButton(
+            onPressed: () async {
+              var res = await get(Uri.parse("https://meme-api.com/gimme"));
+              print(jsonDecode(res.body)['url']);
+              meme = jsonDecode(res.body)['url'];
+              setState(() {});
+            },
+            child: Text("Laugh!!!")));
   }
 }
